03fadcc238b23c650b5f97497b04d126c5da277c
unknown <i9e00417@cxldom00.cathay.ins>
2020-05-12 09:55:16 +0800

    remove theme

--- src/Theme.js
+++ src/Theme.js
@@ -7,64 +7,39 @@ const arcGrey = '#866666'
 export default  createMuiTheme({
     palette: {
         common:{
-            blue: `${arcBlue}`,
-            orange: `${arcOrange}`
+            
         },
         primary:{
-            main: `${arcBlue}`
+           
         },
         secondary:{
-            main: `${arcOrange}`
+          
         }
     },
     typography:{
         h2:{
-            fontFamily:"Relway",
-            fontWeight:700,
-            color:`${arcBlue}`,
-            fontSize: "2.5rem",
-            lineHeight:1.5
 
         },
         h3:{
-            fontFamily:'pacifico',
-            fontSize:'2.5em',
-            fontWeight:300,
-            color:arcBlue
+           
         },
         h4:{
-            fontFamily:"Relway",
-            fontWeight:700,
-            color:`${arcBlue}`,
-            fontSize: "1.75rem",
+         
         },
         body2:{
-            fontSize:'1.25rem',
-            color: arcGrey,
-            fontWeight:300
-
+         
         }, 
         subtitle1:{
-            fontSize: "1.25rem",
-            fontWeight : 300,
-            color : `${arcGrey}`
+           
         },
         subtitle2:{
-            fontSize: "1.25rem",
-            fontWeight : 300,
-            color : 'white'
+            
         },
         tab:{
-            fontFamily:'Raleway',
-            textTransform:'none',
-            fontWeight:700,
-            fontSize: "1.1rem",
+         
         },
         estimate:{
-            fontFamily: "Roboto",
-            fontSize:"1rem",
-            textTransform:'none',
-            color:"white"
+            
         }
     },
     overrides:{
